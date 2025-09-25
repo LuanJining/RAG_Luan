9ee63a6d93aa1c7b3c94c734feaab62bd09f5b29
深情代码大师 <2994718175@qq.com>
2025-09-25 11:43:52 +0800

    Initial commit

---
+++ src/test/java/com/luanjining/rag/RagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luanjining.rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
